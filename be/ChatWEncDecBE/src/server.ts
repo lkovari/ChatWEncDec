74535c1b0c76d43df34f4dbe9cd30b664db62842
LK <laszlo.kovary@gmail.com>
2022-01-22 14:39:05 +0100

    initial commit

---
+++ be/ChatWEncDecBE/src/server.ts
@@ -0,0 +1,86 @@
+import * as express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
+
+const app = express();
+
+// Initialize a simple http server
+const server = http.createServer(app);
+
+// Initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+interface ExtWebSocket extends WebSocket {
+    isAlive: boolean;
+}
+
+function createMessage(content: string, isBroadcast = false, sender = 'NS'): string {
+    return JSON.stringify(new Message(content, isBroadcast, sender));
+}
+
+export class Message {
+    constructor(
+        public content: string,
+        public isBroadcast = true,
+        public sender: string
+    ) { }
+}
+
+wss.on('connection', (ws: WebSocket) => {
+
+    const extWs = ws as ExtWebSocket;
+
+    extWs.isAlive = true;
+
+    ws.on('pong', () => {
+        extWs.isAlive = true;
+    });
+
+    // Connection is up, let's add a simple simple event
+    ws.on('message', (msg: string) => {
+
+        const message = JSON.parse(msg) as Message;
+
+        setTimeout(() => {
+            if (message.isBroadcast) {
+
+                // Send back the message to the other clients
+                wss.clients
+                    .forEach(client => {
+                        if (client != ws) {
+                            client.send(createMessage(message.content, true, message.sender));
+                        }
+                    });
+
+            }
+
+            ws.send(createMessage(`Sent -> ${message.content}`, message.isBroadcast));
+
+        }, 1000);
+
+    });
+
+    // Send immediatly a feedback to the incoming connection    
+    ws.send(createMessage('WebSocket server'));
+
+    ws.on('error', (err) => {
+        console.warn(`Client Disconnected - reason: ${err}`);
+    })
+});
+
+setInterval(() => {
+    wss.clients.forEach((ws: WebSocket) => {
+
+        const extWs = ws as ExtWebSocket;
+
+        if (!extWs.isAlive) return ws.terminate();
+
+        extWs.isAlive = false;
+        ws.ping(null, undefined);
+    });
+}, 10000);
+
+// Server start
+server.listen(process.env.PORT || 8999, () => {
+    console.log(`WebSocket server started on port ${server.address().port} :)`);
+});
---
+++ fe/ChatWEncDecFE/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'ChatWEncDecFE'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('ChatWEncDecFE');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('ChatWEncDecFE app is running!');
+  });
+});
