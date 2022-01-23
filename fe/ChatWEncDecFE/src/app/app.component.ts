34066e2bd7ad3a466d24c1bf8aed1ee6bf6b95a9
LK <laszlo.kovary@gmail.com>
2022-01-23 20:08:32 +0100

    added input field to set other chat partner decryptKey

--- fe/ChatWEncDecFE/src/app/app.component.ts
+++ fe/ChatWEncDecFE/src/app/app.component.ts
@@ -12,7 +12,7 @@ import { EncrptDecryptService } from './services/encdec/encrpt-decrypt.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit  {
-  title = "WebSocket Chat Example With Enc/Dec feature";
+  title = "WebSocket ´Chat´ Example";
   private MSG_CONNECTION_MESSAGE = 'Connected'
   autoResize = false;
   participant = undefined;
@@ -23,7 +23,8 @@ export class AppComponent implements OnInit  {
   webSocket$!: WebSocketSubject<Message>;
   currentUserName!: string;
   connectetButtonTitle = 'Connect';
-  secretKey!: string;
+  encryptKey!: string;
+  decryptKeyDisabled = false;  
 
   constructor(private formBuilder: FormBuilder, private encDecService: EncrptDecryptService) {
     this.dataEntryForm = this.formBuilder.group({
@@ -31,41 +32,57 @@ export class AppComponent implements OnInit  {
         Validators.required, Validators.minLength(this.userNameMinLength),
         Validators.maxLength(this.userNameMaxLength)
       ], updateOn: 'blur' } ],
-      message: [ null ], 
-      updateOn: 'blur'
+      decryptKey: [ {value: null, disabled: this.decryptKeyDisabled} ], updateOn: 'blur',
+      message: [ null ]
     });
   }
 
   
   webSocketConnect() {
+    // if not connected to the server then connecting
     if (!this.webSocket$) {
+      // create 
       this.webSocket$ = new WebSocketSubject('ws://localhost:7777');
+      // replace the button title "Connect" with "Connected"
       if (this.webSocket$) {
         this.connectetButtonTitle = 'Connected';
       }
+      /* former tought, in the map handling the enc/dec etc.
       this.webSocket$.pipe(
         map((mess: Message) => {
           return mess;
         })
       );
+      */
+      // listening WebSocket messages
       this.webSocket$.subscribe((message: Message) => {
-          if (message.content !== this.MSG_CONNECTION_MESSAGE) {
-            const messageObject = JSON.parse(message.content);
-            
-            const digest = this.encDecService.createHMACSHA1Digest(messageObject.textMessage, this.secretKey);            
-            if (digest === messageObject.hmacsha1) {
-              const decrypted = this.encDecService.decrypt(messageObject.textMessage, this.secretKey);
-              message.content = decrypted;
-              this.sentMessages.push(message);
-            }
-          } else {
+        const decryptKey = this.dataEntryForm.get('decryptKey')?.value;
+        // skip Connected notification
+        if (message.content !== this.MSG_CONNECTION_MESSAGE) {
+          // parse message text to object
+          const messageObject = JSON.parse(message.content);
+          // create digest from received textMessage
+          const digest = this.encDecService.createHMACSHA1Digest(messageObject.textMessage, decryptKey);            
+          // compare the digests
+          if (digest === messageObject.hmacsha1) {
+            // decrypt the message
+            const decrypted = this.encDecService.decrypt(messageObject.textMessage, decryptKey);
+            // store message to the message object 
+            message.content = decrypted;
+            // push message to sentMessages (which will display)
             this.sentMessages.push(message);
+          } else {
+
           }
-        },
-        (err) => console.error(err),
-        () => console.info('Completed!')
-      );
-      this.secretKey = this.generateSecretKey();
+        } else {
+          // "Connected" notification simply display
+          this.sentMessages.push(message);
+        }
+      },
+      (err) => console.error(err),
+      () => console.info('Completed!')
+    );
+    this.encryptKey = this.generateSecretKey();
     }
   }
 
@@ -76,8 +93,9 @@ export class AppComponent implements OnInit  {
   private generateSecretKey(): string {
     let numKey = (Math.random() * Number.MAX_SAFE_INTEGER);
     let strKey = btoa('' + numKey);
-    // return strKey;
-    return 'Mjc4ODAxNDAxOTcyMTkxNy41';
+    console.log(`Decrypt key ${strKey}`);
+    return strKey;
+    // return 'Mjc4ODAxNDAxOTcyMTkxNy41';
   }
 
   extractMessage(message: Message): string {
@@ -92,8 +110,8 @@ export class AppComponent implements OnInit  {
     this.currentUserName = this.dataEntryForm.get('userName')?.value;
     let textMessage = this.dataEntryForm.get('message')?.value;
 
-    const encryptedText = this.encDecService.encrypt(textMessage, this.secretKey);
-    const digest = this.encDecService.createHMACSHA1Digest(encryptedText, this.secretKey);
+    const encryptedText = this.encDecService.encrypt(textMessage, this.encryptKey);
+    const digest = this.encDecService.createHMACSHA1Digest(encryptedText, this.encryptKey);
     
     const customMessage = new CustomMessage(encryptedText, digest);    
     const messageObject = JSON.stringify(customMessage);
